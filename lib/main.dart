81af28d924f8f654bb5d36e969ebd17556a525a9
DESKTOP-MR44V0P\Satya <satya0906@gmail.com>
2021-01-13 18:58:38 +0700

    COMMIT : Triple Today's Special

--- lib/main.dart
+++ lib/main.dart
@@ -153,8 +153,110 @@ class _MyHomePageState extends State<MyHomePage> {
                             ),
                           ),
                           color: blue,
+                          boxShadow: [
+                            BoxShadow(
+                                spreadRadius: 0,
+                                offset: Offset(0, 10),
+                                blurRadius: 0,
+                                color: blue.withOpacity(0.4))
+                          ],
                         ),
-                      )
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Expanded(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    image: AssetImage("asset/images/food1.png"),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 15,
+                            ),
+                            Text(
+                              "Yoshimasa Sushi",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.star,
+                                  color: Colors.white,
+                                  size: 17,
+                                ),
+                                Icon(
+                                  Icons.star,
+                                  color: Colors.white,
+                                  size: 17,
+                                ),
+                                Icon(
+                                  Icons.star,
+                                  color: Colors.white,
+                                  size: 17,
+                                ),
+                                Icon(
+                                  Icons.star,
+                                  color: Colors.white,
+                                  size: 17,
+                                ),
+                                Icon(
+                                  Icons.star,
+                                  color: Colors.white,
+                                  size: 17,
+                                ),
+                                Text(
+                                  "250 ratings",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 10,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              "Fresh salmon, unagi, mentai, etc",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 13,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Column(
+                        children: [
+                          Container(
+                            width: MediaQuery.of(context).size.width * 0.35,
+                            height: 165,
+                            padding: EdgeInsets.symmetric(
+                              vertical: 20,
+                              horizontal: 20,
+                            ),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(30),
+                              ),
+                              color: green,
+                            ),
+                          ),
+                        ],
+                      ),
                     ],
                   ),
                 )
